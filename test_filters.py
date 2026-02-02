4f3374af1f5cd48beeaac5727bb56ac800fd7e73
Eranga <erangakavishanka@icloud.com>
2026-02-02 23:27:28 +0530

    tested for mean,gaussian filter as well

--- test_filters.py
+++ test_filters.py
@@ -2,7 +2,11 @@ import cv2
 from processing.filters import apply_filter
 
 img = cv2.imread("assets/samples/test1.png")  # put any image here
-out = apply_filter(img, "median", k=5)
+out = apply_filter(img, "gaussian", k=5, sigma=1.0)
 
-cv2.imwrite("output/test_median.png", out)
-print("Saved output/test_median.png")
+cv2.imwrite("output/test_gaussian.png", out)
+print("Saved output/test_gaussian.png")
+
+
+
+
